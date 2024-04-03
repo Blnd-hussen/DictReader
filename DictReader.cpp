6f01b0d84abb61161ced059a2416f7863f159d55
Blnd Hussen <blndqaladzey12@gmail.com>
2024-04-03 14:44:57 +0300

    Add DictReader class implementation

---
+++ DictReader.cpp
@@ -0,0 +1,75 @@
+#include "DictReader.h"
+
+DictReader::DictReader(const std::string filePath, const char delimiter)
+{
+  // check if a csv file path is provied
+  std::regex re("^.+\\.csv$");
+  if (!std::regex_match(filePath, re))
+    throw std::invalid_argument("Invalid file path provided. provide a valid path to a CSV file.");
+
+  // open the file for reading and check if it exists
+  std::ifstream file(filePath);
+  if (!file.is_open())
+    throw std::runtime_error("Failed to open CSV file. ensure the file exists and you have appropriate permissions.");
+
+  numbLines = 0;
+  std::string line;
+  while (std::getline(file, line))
+  {
+    if (numbLines == 0)
+    {
+      fieldNames = split(line, delimiter);
+    }
+    else
+    {
+      auto rowData = split(line, delimiter);
+      std::map<std::string, std::string> row;
+      for (size_t i = 0; i < fieldNames.size(); ++i)
+      {
+        row[fieldNames[i]] = rowData[i];
+      }
+      rows.push_back(row);
+    }
+    numbLines++;
+  }
+  file.close();
+};
+
+std::vector<std::string> DictReader::split(const std::string str, const char delimiter)
+{
+  std::vector<std::string> tokens;
+  std::string token{};
+
+  for (int i = 0; str[i] != '\0'; ++i)
+  {
+    if (str[i] == delimiter || str[i + 1] == '\0')
+    {
+      if (str[i + 1] == '\0')
+        token += str[i];
+
+      tokens.push_back(token);
+      token = "";
+    }
+    else
+    {
+      token += str[i];
+    }
+  }
+
+  return tokens;
+}
+
+std::vector<std::string> DictReader::getFieldNames()
+{
+  return fieldNames;
+}
+
+std::vector<std::map<std::string, std::string>> DictReader::getRows()
+{
+  return rows;
+}
+
+size_t DictReader::getNumbLines()
+{
+  return numbLines;
+}
---
+++ DictReader.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <iostream>
+#include <regex>
+#include <fstream>
+#include <vector>
+#include <map>
+
+class DictReader
+{
+private:
+  size_t numbLines;
+  std::vector<std::string> fieldNames;
+  std::vector<std::map<std::string, std::string>> rows;
+
+  std::vector<std::string> split(const std::string, const char = ' ');
+
+public:
+  DictReader(const std::string, const char = ',');
+  size_t getNumbLines();
+  std::vector<std::string> getFieldNames();
+  std::vector<std::map<std::string, std::string>> getRows();
+};
