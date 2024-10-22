a579ec03708344faa8f94c65929b3f7c91d4f727
Blnd-hussen <blndqaladzey12@gmail.com>
2024-10-22 20:48:18 +0300

    Add new DictReader::toJsonString method to get the collected data in json format

--- src/DictReader.cpp
+++ src/DictReader.cpp
@@ -1,6 +1,7 @@
 #include "../include/DictReader.hpp"
 #include <fstream>
 #include <regex>
+#include <sstream>
 
 namespace fs = std::filesystem;
 
@@ -9,25 +10,22 @@ DictReader::DictReader(const fs::path filePath, const char delimiter) {
   // * make sure a csv path is provided
   if (filePath.empty() || filePath.extension() != ".csv") {
     throw std::invalid_argument(
-      "Invalid file path provided. provide a valid path to a CSV file. " +
-      filePath.generic_string()
-    );
+        "Invalid file path provided. provide a valid path to a CSV file. " +
+        filePath.generic_string());
   }
 
   // * check if the file exists and is a regular file
   if (!fs::exists(filePath) || !fs::is_regular_file(filePath)) {
-    throw std::runtime_error(
-      filePath.generic_string() + " does not exist or is not regular file"
-    );
+    throw std::runtime_error(filePath.generic_string() +
+                             " does not exist or is not regular file");
   }
 
   // * make sure that file has read permission
   std::ifstream file(filePath);
   if (!file.is_open()) {
     throw std::runtime_error(
-      "Failed to open CSV file: " + filePath.generic_string() +
-      "\nensure that you have appropriate permissions."
-    );
+        "Failed to open CSV file: " + filePath.generic_string() +
+        "\nensure that you have appropriate permissions.");
   }
 
   numbLines = 0;
@@ -62,9 +60,28 @@ auto DictReader::getRows() -> std::vector<std::map<std::string, std::string>> {
 
 auto DictReader::getNumbLines() -> size_t { return numbLines; }
 
-auto DictReader::hasFieldNames() -> bool { return fieldNames.size() > 0; }
+auto DictReader::hasFieldNames() -> bool { return !fieldNames.empty(); }
 
-auto DictReader::hasRows() -> bool { return rows.size() > 0; }
+auto DictReader::hasRows() -> bool { return !rows.empty(); }
+
+auto DictReader::toJsonString() -> std::string {
+  if (!hasFieldNames() || !hasRows()) {
+    return "{}";
+  }
+
+  std::stringstream stringBuilder;
+  for (const auto &row : rows) {
+    stringBuilder << "{";
+    for (size_t j = 0; j < fieldNames.size(); ++j) {
+      const auto currentField = fieldNames[j];
+      stringBuilder << "'" << currentField << "': '" << row.at(currentField)
+                    << (j + 1 == fieldNames.size() ? "'" : "', ");
+    }
+    stringBuilder << "}\n";
+  }
+
+  return stringBuilder.str();
+}
 
 auto split(const std::string str, const char delimiter)
     -> std::vector<std::string> {
@@ -75,8 +92,8 @@ auto split(const std::string str, const char delimiter)
     if (str[i] == delimiter || str[i + 1] == '\0') {
       if (str[i + 1] == '\0') {
         token += str[i];
-      } 
-         
+      }
+
       tokens.push_back(token);
       token = "";
     } else {
