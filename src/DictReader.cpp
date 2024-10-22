a76606ca29bcf5551bb892df094b5457b64173d3
Blnd-hussen <blndqaladzey12@gmail.com>
2024-10-22 21:25:26 +0300

    use regex to parse CSV data to handle data with quoted fields more robustly

--- src/DictReader.cpp
+++ src/DictReader.cpp
@@ -9,25 +9,22 @@ DictReader::DictReader(const fs::path filePath, const char delimiter) {
   // * make sure a csv path is provided
   if (filePath.empty() || filePath.extension() != ".csv") {
     throw std::invalid_argument(
-      "Invalid file path provided. provide a valid path to a CSV file. " +
-      filePath.generic_string()
-    );
+        "Invalid file path provided. provide a valid path to a CSV file. " +
+        filePath.generic_string());
   }
 
   // * check if the file exists and is a regular file
   if (!fs::exists(filePath) || !fs::is_regular_file(filePath)) {
-    throw std::runtime_error(
-      filePath.generic_string() + " does not exist or is not regular file"
-    );
+    throw std::runtime_error(filePath.generic_string() +
+                             " does not exist or is not regular file");
   }
 
   // * make sure that file has read permission
   std::ifstream file(filePath);
   if (!file.is_open()) {
     throw std::runtime_error(
-      "Failed to open CSV file: " + filePath.generic_string() +
-      "\nensure that you have appropriate permissions."
-    );
+        "Failed to open CSV file: " + filePath.generic_string() +
+        "\nensure that you have appropriate permissions.");
   }
 
   numbLines = 0;
@@ -68,20 +65,19 @@ auto DictReader::hasRows() -> bool { return rows.size() > 0; }
 
 auto split(const std::string str, const char delimiter)
     -> std::vector<std::string> {
-  std::vector<std::string> tokens;
-  std::string token{};
 
-  for (int i = 0; str[i] != '\0'; ++i) {
-    if (str[i] == delimiter || str[i + 1] == '\0') {
-      if (str[i + 1] == '\0') {
-        token += str[i];
-      } 
-         
-      tokens.push_back(token);
-      token = "";
-    } else {
-      token += str[i];
+  std::vector<std::string> tokens;
+  std::smatch matches;
+  std::regex re("\"([^\"]*)\"|([^,]+)");
+  
+  std::string line = str;
+  while (std::regex_search(line, matches, re)) {
+    for (int i = 1; i < matches.size(); ++i) {
+      if (matches[i].matched) {
+        tokens.push_back(matches[i].str());
+      }
     }
+    line = matches.suffix().str();
   }
 
   return tokens;
